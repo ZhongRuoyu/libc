f120b0e5a27283f833f717af3b04e7b83eded30e
ZhongRuoyu <zhongruoyu@outlook.com>
2021-03-27 02:27:49 +0800

    iso646.h added

---
+++ include/iso646.h
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2021 Zhong Ruoyu.
+ * This is part of the libc project.
+ * SPDX-License-Identifier: MIT
+ */
+
+/**
+ * @file iso646.h
+ * Alternate spellings.
+ * This header file is designed to be compatible with the C17 standard and
+ * support gcc and clang, except that it is not backward compatible.
+ */
+
+#ifndef __ISO646_H__
+#define __ISO646_H__
+
+#define and     &&
+#define and_eq  &=
+#define bitand  &
+#define bitor   |
+#define compl   ~
+#define not     !
+#define not_eq  !=
+#define or      ||
+#define or_eq   |=
+#define xor     ^
+#define xor_eq  ^=
+
+#endif  /* __ISO646_H__ */
