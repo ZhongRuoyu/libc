a843196a3bc9853f9d3db01982ce99c8b1cf82d1
ZhongRuoyu <zhongruoyu@outlook.com>
2021-03-27 02:26:23 +0800

    stdnoreturn.h added

---
+++ include/stdnoreturn.h
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2021 Zhong Ruoyu.
+ * This is part of the libc project.
+ * SPDX-License-Identifier: MIT
+ */
+
+/**
+ * @file stdnoreturn.h
+ * _Noreturn.
+ * This header file is designed to be compatible with the C17 standard and
+ * support gcc and clang, except that it is not backward compatible.
+ */
+
+#ifndef __STDNORETURN_H__
+#define __STDNORETURN_H__
+
+#define noreturn    _Noreturn
+
+#endif  /* __STDNORETURN_H__ */
